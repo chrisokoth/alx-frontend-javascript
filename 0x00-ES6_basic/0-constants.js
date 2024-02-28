1e5f9b5b093aa5a8713ef5331ffe76c4e96d49cc
chrisokoth <chrisokoth17@gmail.com>
2024-02-28 05:33:05 +0300

    commit

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,12 @@
+// 0-constants.js
+
+// Function taskFirst to indicate preference for using const
+export function taskFirst() {
+    return "I prefer const when I can.";
+  }
+  
+  // Function taskNext to indicate preference for using let
+  export function taskNext() {
+    return "But sometimes let is okay.";
+  }
+  
